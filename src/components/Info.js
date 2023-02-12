a626998ada415a693eb096561bc7298abe43cc5b
mrahmanau <mrahmanlinks@gmail.com>
2023-02-12 01:25:37 -0400

    files ready to upload

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+export default function Info() {
+    return(
+        <div className="info">
+            <img src="https://i.pinimg.com/736x/e4/8d/a0/e48da0993d1737ac2f0b7e0b17d943a6--business-headshots-corporate-headshots.jpg" alt="" />
+                <h2>Mahfuzur Rahman</h2>
+                <h4>Frontend Developer</h4>
+                <p><a href="https://bdwebtutor.com/">Website</a></p>
+                <div className="buttons">
+                    <a href="bd.com">Email</a>
+                    <a href="dd.com">Linkedin</a>
+                </div>
+        </div>
+    )
+}
